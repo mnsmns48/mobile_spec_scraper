a4fd60c38eb5c729fbc986c032493af23e768c24
mnsmns48 <getzboy@list.ru>
2025-04-04 14:37:41 +0300

    fix models - working triggers

--- database/models.py
+++ database/models.py
@@ -20,9 +20,11 @@ class Base(DeclarativeBase):
 
 
 class Product(Base):
-    __table_args__ = (UniqueConstraint('link', name='uix_link'),
-                      Index('idx_link', 'link'),
-                      Index('idx_title_tsv', 'title_tsv', postgresql_using='gin'))
+    __table_args__ = (
+        UniqueConstraint('link', name='uix_link'),
+        Index('idx_link', 'link'),
+        Index('idx_title_tsv', 'title_tsv', postgresql_using='gin'),
+    )
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title_line: Mapped[str]
     title: Mapped[str]
@@ -38,7 +40,9 @@ class Product(Base):
 
 
 class Product_Type(Base):
-    __table_args__ = (Index('idx_kind_tsv', 'kind_tsv', postgresql_using='gin'),)
+    __table_args__ = (
+        Index('idx_kind_tsv', 'kind_tsv', postgresql_using='gin'),
+    )
     id: Mapped[int] = mapped_column(primary_key=True)
     type: Mapped[str] = mapped_column(nullable=False)
     kind: Mapped[list] = mapped_column(ARRAY(String), nullable=False)
@@ -47,7 +51,9 @@ class Product_Type(Base):
 
 
 class Brand(Base):
-    __table_args__ = (Index('idx_brand_depends_tsv', 'brand_depends_tsv', postgresql_using='gin'),)
+    __table_args__ = (
+        Index('idx_brand_depends_tsv', 'brand_depends_tsv', postgresql_using='gin'),
+    )
     id: Mapped[int] = mapped_column(primary_key=True)
     brand: Mapped[str] = mapped_column(nullable=False)
     brand_depends: Mapped[list] = mapped_column(ARRAY(String), nullable=False)
@@ -55,50 +61,48 @@ class Brand(Base):
     products: Mapped[list['Product']] = relationship('Product', back_populates='brand')
 
 
-@event.listens_for(Product.__table__, 'after_create')
-def create_update_title_tsv_trigger(target, connection, **kw):
-    connection.execute(text(
-        f"""
-        CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-        ON {Product.__table__} FOR EACH ROW EXECUTE PROCEDURE
-        tsvector_update_trigger(title_tsv, 'pg_catalog.simple', title);
-        """
-    ))
-
-
-@event.listens_for(Brand.__table__, 'after_create')
-def create_update_kind_tsv_trigger(target, connection, **kw):
-    connection.execute(text(
-        f"""
-        CREATE OR REPLACE FUNCTION update_brand_depends_tsv_function() RETURNS TRIGGER AS $$
-        BEGIN
-            NEW.brand_depends_tsv := to_tsvector('simple', array_to_string(NEW.brand_depends, ' '));
-            RETURN NEW;
-        END;
-        $$ LANGUAGE plpgsql;
-
-        CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-        ON {Brand.__tablename__}
-        FOR EACH ROW
-        EXECUTE FUNCTION update_brand_depends_tsv_function();
-        """
-    ))
-
-
-@event.listens_for(Product_Type.__table__, 'after_create')
-def create_update_kind_tsv_trigger(target, connection, **kw):
-    connection.execute(text(
-        f"""
+@event.listens_for(Product_Type.__table__, "after_create")
+def create_product_type_trigger(target, connection, **kw):
+    connection.execute(text("""
         CREATE OR REPLACE FUNCTION update_kind_tsv_function() RETURNS TRIGGER AS $$
         BEGIN
             NEW.kind_tsv := to_tsvector('simple', array_to_string(NEW.kind, ' '));
             RETURN NEW;
         END;
         $$ LANGUAGE plpgsql;
+        """))
 
-        CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-        ON {ProductType.__tablename__}
+    connection.execute(text("""
+        CREATE TRIGGER tsvectorupdate_product_type BEFORE INSERT OR UPDATE
+        ON product_type
         FOR EACH ROW
         EXECUTE FUNCTION update_kind_tsv_function();
-        """
-    ))
+        """))
+
+
+@event.listens_for(Brand.__table__, "after_create")
+def create_brand_trigger(target, connection, **kw):
+    connection.execute(text("""
+        CREATE OR REPLACE FUNCTION update_brand_depends_tsv_function() RETURNS TRIGGER AS $$
+        BEGIN
+            NEW.brand_depends_tsv := to_tsvector('simple', array_to_string(NEW.brand_depends, ' '));
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+        """))
+
+    connection.execute(text("""
+        CREATE TRIGGER tsvectorupdate_brand BEFORE INSERT OR UPDATE
+        ON brand
+        FOR EACH ROW
+        EXECUTE FUNCTION update_brand_depends_tsv_function();
+        """))
+
+
+@event.listens_for(Product.__table__, "after_create")
+def create_product_trigger(target, connection, **kw):
+    connection.execute(text("""
+        CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+        ON product FOR EACH ROW EXECUTE PROCEDURE
+        tsvector_update_trigger(title_tsv, 'pg_catalog.simple', title);
+        """))
