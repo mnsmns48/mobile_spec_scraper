e4144b2c97c7b35668895b3ccc2094d85d06cf3e
mnsmns48 <getzboy@list.ru>
2025-04-18 22:10:47 +0300

    add user_manager dependencies

---
+++ api/dependencies/user_manager.py
@@ -0,0 +1,13 @@
+from typing import Annotated, TYPE_CHECKING
+
+from fastapi import Depends
+
+from api.dependencies.users import get_user_db
+from core.auth.user_manager import UserManager
+
+if TYPE_CHECKING:
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
+
+async def get_user_manager(users_db: Annotated["SQLAlchemyUserDatabase", Depends(get_user_db)]):
+    yield UserManager(users_db)
