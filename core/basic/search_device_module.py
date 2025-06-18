07c55fdb308dc038a660411c5dcc7211a7633c96
r_mns <getzboy@list.ru>
2025-06-18 08:05:11 +0300

    update all_items_by_brand

--- core/basic/search_device_module.py
+++ core/basic/search_device_module.py
@@ -108,7 +108,7 @@ async def search_devices(session: AsyncSession,
 def format_tsquery(input_string: str) -> list:
     cleaned = input_string.lower().replace("+", " plus ")
     cleaned = BAD_CHARS_RE.sub(" ", cleaned)
-    tokens = [tok for tok in cleaned.split() if len(tok) > 1]
+    tokens = [tok for tok in cleaned.split()]
     return tokens
 
 
