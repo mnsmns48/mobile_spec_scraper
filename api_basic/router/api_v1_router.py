89e12841e455bba946e28cdb207c5270e602aa0d
mnsmns48 <getzboy@list.ru>
2026-07-01 12:55:05 +0300

    add in template_router

--- api_basic/router/api_v1_router.py
+++ api_basic/router/api_v1_router.py
@@ -1,18 +1,12 @@
 import asyncio
-import re
 
 from fastapi import APIRouter, Depends, Form
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.requests import Request
-from starlette.responses import HTMLResponse
-
-from api_basic.errors import ValidationFailedException
 from api_basic.schemas import ItemList
-from core.basic.logic_module import add_new_one, get_nanoreview_list_for_parsing
+from core.basic.logic_module import add_new_one
 from core.basic.search_device_module import search_product_by_model, search_devices, search_device_forced
 from database.engine import db
-from database.models import Brand, Product_Type
-from templates import templates
+from database.models import Brand
 
 api_v1_router = APIRouter(tags=['Api V1 UI USER'])
 
@@ -47,28 +41,3 @@ async def add_info(url: str = Form(...)):
     async with db.scoped_session() as session:
         result = await add_new_one(session=session, url=url)
     return result
-
-
-@api_v1_router.post("/submit_one_url", response_class=HTMLResponse)
-async def submit_link(request: Request, url: str = Form(...)):
-    if not url:
-        return templates.TemplateResponse("add_url.html", {"request": request})
-    result = await add_info(url)
-    if result.get('error'):
-        return templates.TemplateResponse("error.html", {"request": request, "result": result['response']})
-    return templates.TemplateResponse("fetch_url.html", {"request": request, "result": result})
-
-
-@api_v1_router.post("/submit_pars_all", response_class=HTMLResponse)
-async def submit_pars_all(request: Request):
-    form_data = await request.form()
-    url_for_pars = form_data.get("url")
-    pattern = r'^https://nanoreview\.net/(ru|en)/.+/all-.+$'
-    if bool(re.match(pattern, url_for_pars)):
-        url_list = await get_nanoreview_list_for_parsing(url=url_for_pars)
-        if url_list:
-            return templates.TemplateResponse("link_for_pars.html", {"request": request, "url_for_pars": url_list})
-        else:
-            return templates.TemplateResponse("no_items.html", {"request": request})
-    else:
-        raise ValidationFailedException(message="URL does not match the expected format")
--- api_basic/router/template_router.py
+++ api_basic/router/template_router.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+import re
+
+from fastapi import APIRouter, Form
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 
+from api_basic.errors import ValidationFailedException
+from api_basic.router.api_v1_router import add_info
+from core.basic.logic_module import get_nanoreview_list_for_parsing
 from templates import templates
 
 welcome_router = APIRouter(tags=['Welcome'])
@@ -26,3 +31,28 @@ async def add_url(request: Request):
 @template_router.get("/engineering-menu/")
 async def get_engineering_menu(request: Request):
     return templates.TemplateResponse("engineering_menu.html", {"request": request})
+
+
+@template_router.post("/submit_one_url", response_class=HTMLResponse)
+async def submit_link(request: Request, url: str = Form(...)):
+    if not url:
+        return templates.TemplateResponse("add_url.html", {"request": request})
+    result = await add_info(url)
+    if result.get('error'):
+        return templates.TemplateResponse("error.html", {"request": request, "result": result['response']})
+    return templates.TemplateResponse("fetch_url.html", {"request": request, "result": result})
+
+
+@template_router.post("/submit_pars_all", response_class=HTMLResponse)
+async def submit_pars_all(request: Request):
+    form_data = await request.form()
+    url_for_pars = form_data.get("url")
+    pattern = r'^https://nanoreview\.net/(ru|en)/.+/all-.+$'
+    if bool(re.match(pattern, url_for_pars)):
+        url_list = await get_nanoreview_list_for_parsing(url=url_for_pars)
+        if url_list:
+            return templates.TemplateResponse("link_for_pars.html", {"request": request, "url_for_pars": url_list})
+        else:
+            return templates.TemplateResponse("no_items.html", {"request": request})
+    else:
+        raise ValidationFailedException(message="URL does not match the expected format")
